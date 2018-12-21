6fe5d2acfe06ff5021db40866f18520b58f6b107
Abhinav <abhinav95@hotmail.com>
2018-12-22 02:58:37 +0530

    ToolBar Nav Items & Created SideDrawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Aux from '../../hoc/Aux';
 import classes from './Layout.css';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 import Toolbar from '../Navigation/Toolbar/Toolbar'
 
 const layout = props => (
   <Aux>
 
     <Toolbar />
+    <SideDrawer/>
     <main className={classes.Content}>{props.children}</main>
   </Aux>
 );
